2c4c0cb556aa8629da9c12a1db51034c98943eaa
Lexter <44911576+Lexterl33t@users.noreply.github.com>
2022-12-04 18:57:49 +0100

    4

--- parser/parser.go
+++ parser/parser.go
@@ -16,6 +16,28 @@ func NewParser() *Parser_t {
 	return &Parser_t{}
 }
 
+func (p *Parser_t) CheckToken(tok token.Token_t) bool {
+	if p.CurrentToken.Kind == tok.Kind {
+		return true
+	}
+
+	return false
+}
+
+func (p *Parser_t) CheckPeekToken(tok token.Token_t) bool {
+	if p.PeekToken.Kind == tok.Kind {
+		return true
+	}
+
+	return false
+}
+
+func (p *Parser_t) Match() {
+
+}
+
 func (p *Parser_t) Parse(sourceCode string) {
+
 	p.Lex = lexer.NewLexer(sourceCode)
+
 }
